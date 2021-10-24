16f22698e040551dcf6fec79ace365852c64702e
SWin <sigurk16@uia.no>
2021-10-24 18:59:33 +0200

    Added PWM

--- main.cpp
+++ main.cpp
@@ -1,22 +1,47 @@
 /*
- * helloAtmega.cpp
+ * prosjekt1oppgave3.cpp
  *
- * Created: 24.10.2021 16:53:08
+ * Created: 14.10.2021 12:37:24
  * Author : S21H
  */ 
 
-#include <avr/io.h>
+#define F_CPU 20000000UL // Forteller delay hvor rask mikrochip kjører, ATmega 168 = 20 mhz
 
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+void initDTimer(){
+	DDRD |= 0x0;										// Init DDRD, god praksis
+	DDRD |= 1 << PD6;									// Init PD6
+	TCCR0A |= 1 << WGM00 | 1 << WGM01 | 1 << COM0A1;    // Opp til 0xFF-nullstilles...
+	TCCR0B |= 1 << CS00;								// Prescaler 
+}
+
+void SetPWMoutput(int duty)
+{
+	OCR0A = duty;
+}
 
 int main(void)
 {
-    DDRB |= 0x0;				// Init
-	DDRB |= 1 << 1;				// Aktiver
-	PORTB |= 1 << 1;			// Sett høy
-	// PORTB &= ~(1 << 1);		// Sett lav
-	
+	initDTimer();
+	int brightness = 0;									
+
     while (1) 
     {
+		for(brightness = 0; brightness < 40; brightness++)
+		{
+			SetPWMoutput(brightness);
+			_delay_ms(1);
+		}
+		for(brightness = 40; brightness > 0; brightness--)
+		{
+			SetPWMoutput(brightness);
+			_delay_ms(1);
+		}
+		
+
     }
 }
 
